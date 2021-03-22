30891d5a0f95b442a33a1466490a1dec575aa69b
vlo-rte <valerie.longa@rte-france.com>
2021-03-22 16:21:32 +0100

    first commit

---
+++ src/test/java/com/rtetdd/bowling/ScorerTests.java
@@ -0,0 +1,11 @@
+package com.rtetdd.bowling;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScorerTests {
+	@Test
+	public void score_a_zero_au_depart(){
+		Assert.assertEquals();
+	}
+}
